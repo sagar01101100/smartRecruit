20b8137121931b95b409b6c0a12305acff6c4f32
sagar01101100 <sagar01101100@gmail.com>
2024-03-01 20:02:26 +0530

    ...

---
+++ applicant_auth.php
@@ -0,0 +1,43 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer\src\Exception.php';
+require 'phpmailer\src\PHPMailer.php';
+require 'phpmailer\src\SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'iitprecruitment@gmail.com';                     //SMTP username
+    $mail->Password   = 'sokdsnmqhqxzlllk';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('iitprecruitment@gmail.com', 'IIT Patna');
+    $mail->addAddress('sagar01101100@gmail.com', 'Sagar');     //Add a recipient
+   
+    $mail->addReplyTo('iitprecruitment@gmail.com', 'IIT Patna');
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Verify Your Email';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
